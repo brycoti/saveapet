a998ad71d191e0cbc72a6ab174606a41080b6aa1
Arnau B. <arnau_b17@hotmail.com>
2024-04-12 16:04:41 +0200

    card

--- frontend/src/pages/Inicio.jsx
+++ frontend/src/pages/Inicio.jsx
@@ -1,11 +1,31 @@
-
+import perro from '../../public/perro.png'
 
 
 const Inicio = () => {
+
+
     return (
 
         <>
-
+            <div className="flex flex-col box-border h-screen justify-between">
+                <div className="flex justify-between p-3 h-1/10">
+                    <div>nombre</div>
+                    <div>inicio/logout</div>
+                    <div>filtros</div>
+                </div>
+                <div className="flex relative grow">
+                    <div className="border border-black absolute"><button>left</button></div>
+                    <div className="flex justify-center items-center h-full w-full"><img src={perro} alt="imagen" /></div>
+                    {/* <div className="bg-contain bg-no-repeat bg-center h-screen w-full" style={{ backgroundImage: "url('../../public/perro.png')" }}></div> */}
+                    <div className="border border-black absolute"><button>right</button></div>
+                </div>
+                <div className="flex justify-between p-3 h-1/10">
+                    <div>opciones</div>
+                    <div>perfil</div>
+                    <div>likes</div>
+                    <div>link center</div>
+                </div>
+            </div >
         </>
     )
 
