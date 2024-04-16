5fcb0dfc6cb2cb04d9b9359e1d311de52d3b0b79
MartaGranja <mgn9907@gmail.com>
2024-04-16 16:22:13 +0200

    inicio plantilla

--- frontend/src/pages/Inicio.jsx
+++ frontend/src/pages/Inicio.jsx
@@ -1,12 +1,28 @@
-
-
-
 const Inicio = () => {
+
+
     return (
 
         <>
-            <h1>Esto es el inicio</h1>
-            <a href="/profile">al perfil</a>
+            <div className="flex flex-col box-border h-screen justify-between">
+                <div className="flex justify-between p-3 h-1/10">
+                    <div>save a pet</div>
+                    <div>inicio/logout</div>
+                    <div>filtros</div>
+                </div>
+                <div className="flex relative grow">
+                    <div className="border border-black absolute"><button>left</button></div>
+                    <div className="flex justify-center items-center h-full w-full"><img src="https://img.freepik.com/foto-gratis/perro-pug-aislado-fondo-blanco_2829-11416.jpg" alt="imagen" /></div>
+                    {/* <div className="bg-contain bg-no-repeat bg-center h-screen w-full" style={{ backgroundImage: "url('../../public/perro.png')" }}></div> */}
+                    <div className="border border-black absolute"><button>right</button></div>
+                </div>
+                <div className="flex justify-between p-3 h-1/10">
+                    <div>opciones</div>
+                    <div>perfil</div>
+                    <div>likes</div>
+                    <div>link center</div>
+                </div>
+            </div >
         </>
     )
 
