07a69e2184968c0a0f9bde34dbadd3021344cd0a
adriigs98 <147430658+adriigs98@users.noreply.github.com>
2024-04-16 16:57:24 +0200

    d

--- api/Controllers/centerController.js
+++ api/Controllers/centerController.js
@@ -15,7 +15,7 @@ const registerCenter = async (req, res, Center) => {
       }
       const center = await Center.create({ name, email, password, phonenumber, web, city, address }); // Crea l'usuari amb les dades proporcionades
   
-      res.status(201).json({message: {id: center.id, name: center.name, email: center.email, phonenumber : center.phonenumber, web: center.web, city: center.city, addres: center.address}}); // Retorna l'usuari creat amb el codi d'estat 201 (Creat)
+      res.status(201).json({message: {userId: center.id, name: center.name, email: center.email, phonenumber : center.phonenumber, web: center.web, city: center.city, addres: center.address}}); // Retorna l'usuari creat amb el codi d'estat 201 (Creat)
     } catch (error) {
       res.status(500).json({ error: error.message }); // Retorna error 500 amb el missatge d'error
     }
@@ -23,18 +23,23 @@ const registerCenter = async (req, res, Center) => {
   
 const newPet =  async (req, res, next, Center, Pet) => {
     try {
-      const center = await Center.findByPk(req.userId); // Cerca l'usuari pel seu ID
+      const center = await Center.findByPk(req.id); // Cerca l'usuari pel seu ID
       if (!center) {
         return res.status(500).json({ error: 'Center no trobat' }); // Retorna error 500 si no es troba l'usuari
       }
-      const { name, breed, age } = req.body;
+      const { name, breed, age, size, temper, dogs_friendly, kids_friendly, urgency } = req.body;
       if (!name || !breed || !age) {
-        return res.status(400).json({ error: 'Nom issue, tipus i ID projecte requerit' }); // Retorna error 400 si no es proporcionen el nom, email o contrasenya
+        return res.status(400).json({ error: 'todos los campos son necesarios' }); // Retorna error 400 si no es proporcionen el nom, email o contrasenya
       }
       const item = await Pet.create({
         name,
         breed,
         age,
+        size,
+        temper,
+        dogs_friendly,
+        kids_friendly,
+        urgency,
         CenterId: req.userId
       })
       res.status(201).json(item); // Retorna l'usuari creat amb el codi d'estat 201 (Creat)
@@ -55,7 +60,7 @@ const newPet =  async (req, res, next, Center, Pet) => {
         }
         const token = jwt.sign({ userId: user.id, userName: user.name }, SECRET_KEY, { expiresIn: '2h' }); // Genera un token JWT vàlid durant 2 hores
         res.cookie('token', token, { httpOnly: false, maxAge: 7200000 }); // Estableix el token com una cookie
-        res.json({id: user.id, name: user.name, email: user.email, phonenumber : user.phonenumber, web: user.web, city: user.city, address: user.address}); // Retorna missatge d'èxit
+        res.json({userId: user.id, name: user.name, email: user.email, phonenumber : user.phonenumber, web: user.web, city: user.city, address: user.address}); // Retorna missatge d'èxit
       } catch (error) {
 
         res.status(500).json({ error: error.message }); // Retorna error 500 amb el missatge d'error
